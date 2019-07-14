ee9858bd4eb7b0f4477bf3472e8fd4f4b69b2773
Alexandre Karpov <alexakarpov@gmail.com>
2019-07-14 14:48:26 -0400

    limit

--- rotate.py
+++ rotate.py
@@ -2,13 +2,14 @@
 
 import boto3, sys
 
-if len(sys.argv) != 3:
-    print("Usage: rotate <bucket> <prefix>")
+if len(sys.argv) < 4:
+    print("Usage: rotate <bucket> <prefix> <limit>")
     sys.exit(1)
 
 client = boto3.client('s3')
 bucket = sys.argv[1]
 prefix = sys.argv[2]
+limit = int(sys.argv[3])
 
 res = client.list_objects_v2(Bucket=bucket, Prefix=prefix)
 print(res)
@@ -23,8 +24,8 @@ print("Found " + str(file_count) + " S3 files")
 
 earliest = min(files, key=lambda k: k['LastModified'])
 
-if (file_count < 3):
-    print("Would delete %s if count was more than 3" % earliest['Key'])
+if (file_count < int(limit)):
+    print("Would delete %s if count was more than %d" % (earliest['Key'], limit))
 else:
     print("Deleting " + str(earliest['Key']))
     client.delete_object(Bucket=bucket, Key=earliest['Key'])
